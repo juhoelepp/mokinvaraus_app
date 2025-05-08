52821c389cd4fbb9823107fe26e252e38e6556fb
codeViltsu <ville.lehikoinen1000@gmail.com>
2025-05-08 15:46:55 +0300

    raportointi.java
    Raportoinnin olio

---
+++ src/main/java/ohii/mokinvaraus_app/raportointi.java
@@ -0,0 +1,12 @@
+package ohjelmistotuotanto1.ot1varaus;
+
+import java.io.Serializable;
+
+public class Raportointi implements Serializable{
+
+    private Double yhteistuotto;
+
+    private Double Tpervaraus;
+
+    private int Vrkpervaraus;
+}
