0e3b5190f1dbd26fa78c35410dd5497af5e2fcc6
codeViltsu <ville.lehikoinen1000@gmail.com>
2025-05-14 13:10:00 +0300

    scenevaihto.java
    ikkunoiden vaihto

---
+++ src/main/java/ot1/mokinvaraus_app/scenevaihto.java
@@ -0,0 +1,35 @@
+package ot1.mokinvaraus_app;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class sivujenvaihto {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void vaihdaikkunaan1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Kansilehti.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 1000, 700);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void vaihdaikkunaan2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 1000, 700);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
