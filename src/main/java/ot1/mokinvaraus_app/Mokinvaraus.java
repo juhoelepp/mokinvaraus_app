1c867f3adf27a2ec0afab6949431974edecae5dd
codeViltsu <ville.lehikoinen1000@gmail.com>
2025-05-14 13:11:25 +0300

    Update Mokinvaraus.java

--- src/main/java/ot1/mokinvaraus_app/Mokinvaraus.java
+++ src/main/java/ot1/mokinvaraus_app/Mokinvaraus.java
@@ -2,6 +2,7 @@ package ot1.mokinvaraus_app;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,8 +11,8 @@ import java.io.IOException;
 public class Mokinvaraus extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Mokinvaraus.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Parent root = FXMLLoader.load(getClass().getResource("Kansilehti.fxml"));
+        Scene scene = new Scene(root);
         stage.setTitle("Mökkien varausohjelma");
         stage.setScene(scene);
         stage.show();
